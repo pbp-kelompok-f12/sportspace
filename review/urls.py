a76f86682fede9aa5d4f9b1a24147874309a7ed5
Fidel <149506792+FidelAkilah@users.noreply.github.com>
2025-10-24 22:42:15 +0700

    added review and book now button

--- review/urls.py
+++ review/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('my/', views.my_reviews, name='my_reviews'),
     path('edit/<int:pk>/', views.edit_review, name='edit_review'),
     path('delete/<int:pk>/', views.delete_review, name='delete_review'),
+    path('all/<int:id>/', views.all_reviews, name='all_reviews'),
 ]
