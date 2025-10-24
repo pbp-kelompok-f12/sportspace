b4ad7b22c8ca0bf8c18cf3017aba723fcf4bb6ad
Fidel <149506792+FidelAkilah@users.noreply.github.com>
2025-10-24 21:37:36 +0700

    edited

--- review/models.py
+++ review/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from home.models import LapanganPadel
+import review
 
 # Create your models here.
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     lapangan = models.ForeignKey(LapanganPadel, on_delete=models.CASCADE, related_name="reviews")
-    rating = models.PositiveIntegerField()
+    rating = models.FloatField(default=0.0)
     comment = models.TextField()
     anonymous = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
